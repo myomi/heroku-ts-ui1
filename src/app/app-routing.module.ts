e69a9c25dfbc8d938658c17540f7f5ebf7045905
MITSUYA MYOUCHIN <m.myouchin@nakamura-joho.co.jp>
2019-06-02 17:16:19 +0900

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then(m => m.LoginModule)
+  },
+  {
+    path: "callback",
+    loadChildren: () =>
+      import("./pages/callback/callback.module").then(
+        m => m.CallbackModule
+      )
+  },
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./pages/home/home.module").then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/callback/callback.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { CallbackComponent } from "./callback.component";
+
+@NgModule({
+  declarations: [CallbackComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: "", component: CallbackComponent }])
+  ]
+})
+export class CallbackModule {}
